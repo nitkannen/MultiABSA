af83ece379a0b7cfd519fd33340c5d9960031820
nitkannen <nithishkannen@gmail.com>
2022-11-22 13:15:20 +0530

    preprocessing and training scripts added

---
+++ preprocessor.py
@@ -0,0 +1,357 @@
+from torch.utils.data import Dataset, DataLoader
+from collections import OrderedDict
+import os
+import torch
+
+sent_map = {}
+sent_map['POS'] = 'positive'
+sent_map['NEU'] = 'neutral'
+sent_map['NEG'] = 'negative'
+
+
+def read_data(path, k_shot):
+
+	sents = open( path + '.sent', 'r')
+	sentences = sents.readlines()
+	tups = open(path +  '.tup', 'r')
+	tuples = tups.readlines()
+
+	if k_shot > 0:
+		sentences = sentences[:k_shot]  ## Temporally taking just the first K examples
+		#print(sentences)
+		tuples = tuples[:k_shot]  ## Temporally taking just the first K examples
+
+	return sentences, tuples
+
+
+def generate_target(d, absa_task = 'aste'):
+	"""
+	takes a aspect triple dictionary and linearizes it
+	"""
+	if absa_task == 'ae':
+		summary = ""
+		for item in d:
+			summary += '<aspect> '
+			summary += item + ' '
+		
+		return summary.strip()
+				
+	elif absa_task == 'oe':
+		summary = ""
+		for item in d:
+			summary += '<opinion> '
+			summary += item + ' '
+		
+		return summary.strip()
+
+	elif absa_task == 'pair':
+		summary = ""
+		if len(d.items()) == 0:
+			return summary
+		for items in d.items():
+			summary += '<aspect> '
+			summary += items[0] + ' '
+			for opinion in items[1]:
+				summary += '<opinion> '
+				summary += opinion + ' '
+
+		return summary.strip()
+				
+	elif absa_task == 'aesc':
+		summary = ""
+		if len(d.items()) == 0:
+			return summary
+		for items in d.items():
+			summary += '<aspect> '
+			summary += items[0] + ' '
+			for sentiment in items[1]:
+				summary += '<sentiment> '
+				summary += sentiment + ' '
+		return summary.strip()
+		
+	elif absa_task == 'aste':
+		summary = ""
+		if len(d.items()) == 0:
+			return summary
+		for items in d.items():
+			summary += '<triplet> '
+			summary += items[0] + ' '
+			for opinion in items[1]:
+				summary += '<opinion> '
+				summary += opinion[0] + ' '
+				summary += '<sentiment> '
+				summary += sent_map[opinion[1]] + ' '
+
+		return summary.strip()
+	else:
+		raise Exception("Unsupported ABSA task")
+	
+
+def generate_aspect_list(tuples, sentence):
+	aspects = tuples.split('|')
+	aspects = [asp.strip() for asp in aspects]
+	aspects = list(set(aspects))
+	ordered_aspects = []
+	for aspect in aspects:
+		ordered_aspects.append( ( sentence.find(aspect.strip())  , aspect) )
+
+	ordered_aspects = sorted(ordered_aspects)
+
+	aspects = [asp[1] for asp in ordered_aspects]
+
+	return aspects
+
+def generate_opinion_list(tuples, sentence):
+	opinions = tuples.split('|')
+	opinions = [op.strip() for op in opinions]
+	opinions = list(set(opinions))
+
+	ordered_opinions = []
+	for opinion in opinions:
+		ordered_opinions.append( ( sentence.find(opinion.strip())  , opinion) )
+
+	ordered_opinions = sorted(ordered_opinions)
+
+	opinions = [op[1] for op in ordered_opinions]
+	return opinions
+
+def generate_pair_dict(tuples, sentence):
+	"""
+	takes a set of tuples and generates aspect opinion pair dictionary
+	"""
+	pairs = tuples.split('|')
+	d = OrderedDict()
+	ordered_pairs = []
+	for pair in pairs:
+		a, o = pair.split(';')
+		ordered_pairs.append( ( sentence.find(a.strip()), sentence.find(o.strip())  , pair) )
+	
+
+	ordered_pairs = sorted(ordered_pairs)
+	
+	for pair in ordered_pairs:
+		a, o = pair[2].split(';')
+		if(a.strip() in d.keys()):
+			d[a.strip()].append(o.strip())
+		else:
+			d[a.strip()] = []
+			d[a.strip()].append(o.strip())
+	
+	return d 
+
+def generate_aesc_dict(tuples, sentence):
+	"""
+	takes a set of tuples and generates aspect sentiment pair dictionary
+	"""
+	aescs = tuples.split('|')
+	d = OrderedDict()
+	ordered_pairs = []
+	for aesc in aescs:
+		a, s = aesc.split(';')
+		ordered_pairs.append( ( sentence.find(a.strip())  , aesc) )
+
+	ordered_pairs = sorted(ordered_pairs)
+	
+	for pair in ordered_pairs:
+		a, s = pair[1].split(';')
+		if(a.strip() in d.keys()):
+			d[a.strip()].append(s.strip())
+		else:
+			d[a.strip()] = []
+			d[a.strip()].append(s.strip())
+	
+	return d 
+
+
+def generate_triplet_dict(tuples, sentence):
+	"""
+	takes a set of tuples and generates triplet dictionary
+	"""
+	triplets = tuples.split('|')
+	d = OrderedDict()
+	ordered_triplets = []
+	for triplet in triplets:
+		#print(triplet)
+		a, o, _ = triplet.split(';')
+		ordered_triplets.append( ( sentence.find(a.strip()), sentence.find(o.strip())  , triplet) )
+	
+	#print(ordered_triplets)
+	ordered_triplets = sorted(ordered_triplets)
+	#print(ordered_triplets)
+	
+	for triplet in ordered_triplets:
+		a, o, s = triplet[2].split(';')
+		if(a.strip() in d.keys()):
+			d[a.strip()].append((o.strip(), s.strip()))
+		else:
+			d[a.strip()] = []
+			d[a.strip()].append((o.strip(), s.strip()))
+	
+	return d 
+
+
+def get_transformed_data(sentences_list, tuples_list, absa_task):
+	"""
+	Preprocess the raw data into Generative Targets
+	"""
+	inputs = []
+	targets = []    
+	
+	for i in range(len(sentences_list)):
+		
+		sent = sentences_list[i].strip()
+		tup = tuples_list[i]
+		if absa_task == 'ae':
+			tup_dict = generate_aspect_list(tup, sent)
+
+		elif absa_task == 'oe':
+			tup_dict = generate_opinion_list(tup, sent)
+
+		elif absa_task == 'pair':
+			tup_dict = generate_pair_dict(tup, sent)
+
+		elif absa_task == 'aesc':
+			tup_dict = generate_aesc_dict(tup, sent)
+		
+		if absa_task == 'aste':
+			tup_dict = generate_triplet_dict(tup, sent)
+
+		target = generate_target(tup_dict, absa_task)
+		inputs.append(sent)
+		targets.append(target)
+
+	return inputs, targets
+
+
+class ABSA_Dataset(Dataset):
+
+	def __init__(self, tokenizer, data_path , task, absa_task = 'aste', k_shot =-1, max_len=128):
+		# 'data/aste/rest16/train.txt'
+		self.data_path = data_path
+		self.task = task
+		self.max_len = max_len
+		self.tokenizer = tokenizer
+		self.k_shot = k_shot
+		self.absa_task = absa_task
+		
+		self.inputs = []
+		self.targets = []
+		self.input_tags = []
+		self.trip_counts = []
+
+		self._build_examples()
+
+	
+	def __len__(self):
+		return len(self.inputs)    
+
+	
+	def __getitem__(self, index):
+		source_ids = self.inputs[index]["input_ids"].squeeze()
+		target_ids = self.targets[index]["input_ids"].squeeze()
+
+		src_mask = self.inputs[index]["attention_mask"].squeeze()      # might need to squeeze
+		target_mask = self.targets[index]["attention_mask"].squeeze()  # might need to squeeze
+		op_tags = self.input_tags[index].squeeze()
+		triplet_count = self.trip_counts[index]
+
+		return {"source_ids": source_ids, "source_mask": src_mask, 
+		"target_ids": target_ids, "target_mask": target_mask, 
+		"op_tags": op_tags, "triplet_count": triplet_count
+		}
+
+
+	def get_tags(self, text, tuples):
+		
+		tuples.split('|')
+		triplets = tuples.split('|')
+		target_tokens = []
+		for triplet in triplets:
+			a, o, _ = triplet.split(';')
+			target_tokens.append(o.strip())
+
+		tokens = self.tokenizer.tokenize(text.strip())
+		target = [0 for i in range(len(tokens))]
+
+		for target_token in target_tokens:
+
+			sub_tok = self.tokenizer.tokenize(target_token)
+
+			if(len(sub_tok) == 0):
+				continue
+
+			for idx in range(len(tokens) + 1 - len(sub_tok)):
+				start_token = tokens[idx]
+				match = True
+
+				if sub_tok[0] == start_token:
+					for j in range(idx, idx + len(sub_tok)):
+						if sub_tok[j - idx] != tokens[j]:
+							match = False
+					if match:
+						target[idx] = 1 ########## 1 == 'B'
+						for k in range(idx + 1, idx + len(sub_tok)):
+							target[k] = 2 ###########  2 == 'I'
+
+		return target
+
+	
+	def get_all_tags(self, sentences_list, tuples_list):
+		"""
+		Preprocess the raw data into tags for opinion
+		"""
+		tags = []
+		
+		for i in range(len(sentences_list)):
+			
+			sent = sentences_list[i].strip()
+			tup = tuples_list[i]
+			t = self.get_tags(sent, tup)
+			tags.append(t)
+
+		return tags
+		
+	
+	def count_triplets(self, tuples_list):
+
+		trip_count = []
+
+		for i in range(len(tuples_list)):
+			trip_count.append( len( tuples_list[i].split('|') ) )
+
+		return trip_count
+
+
+	def _build_examples(self):
+
+		sentences, tuples = read_data(self.data_path, self.k_shot)
+		inputs, targets = get_transformed_data(sentences, tuples, self.absa_task)
+		input_tags = self.get_all_tags(sentences, tuples)  ### pad this and letzgoooo
+		trip_counts = self.count_triplets(tuples)
+
+		for i in range(len(inputs)):
+
+			input = inputs[i]
+			target = targets[i]
+			input_tag = input_tags[i]
+			trip_count = trip_counts[i]
+
+			tokenized_input = self.tokenizer(
+			  [input], max_length=self.max_len, pad_to_max_length=True, truncation=True,
+			  return_tensors="pt",
+			)
+			
+			with self.tokenizer.as_target_tokenizer():
+				tokenized_target = self.tokenizer(
+				[target], max_length=self.max_len, pad_to_max_length=True, truncation=True,
+				return_tensors="pt"
+			)
+
+			input_tag = input_tag + [0] * (self.max_len - len(input_tag))
+			input_tag = torch.tensor(input_tag)
+			trip_count = torch.tensor(trip_count)
+
+			self.input_tags.append(input_tag)
+			self.inputs.append(tokenized_input)
+			self.targets.append(tokenized_target)
+			self.trip_counts.append(trip_count)
---
+++ utils.py
@@ -0,0 +1,445 @@
+
+def correct_spaces(result):	
+	
+	for i in range(len(result)):
+		s = ''
+		prev = ''
+		for char in result[i]:
+			if char == '<':
+				s += ' ' + char
+			elif char == "'":
+				if prev == 'n':
+					s = s[:-1] + ' ' + prev + char
+				else:
+					s += ' ' + char
+			else:
+				s += char
+
+			prev = char
+
+		result[i] = s
+
+	return result
+
+
+def post_process(text, starter):
+    	
+	text = text.strip()
+	if len(text) > len(starter):
+		if text[:len(starter)] != starter:
+			text = starter + ' ' + text
+	
+	return text
+
+
+""" adapted from https://github.com/Babelscape/rebel/blob/main/src/utils.py"""
+
+def decode_pred_aspects(text): # asp
+
+    starter = '<aspect>'
+    text = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    text = text.replace("<extra_id_-1>", starter).replace("<extra_id_-2>", starter ).replace("<extra_id_-3>", starter)
+    text_processed = post_process(text, starter)
+    text_processed = text_processed.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    aspects = []
+    for token in text_processed.split():
+        if token.startswith('<'):
+            continue
+        else:
+            if token.strip() not in aspects:
+                aspects.append(token.strip())
+
+    return aspects
+			
+
+
+def decode_pred_opinions(text): # op
+
+    starter  = '<opinion>'
+    text = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    text = text.replace("<extra_id_-1>", starter).replace("<extra_id_-2>", starter ).replace("<extra_id_-3>", starter)
+    text_processed = post_process(text, starter)
+    text_processed = text_processed.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    opinions = []
+    for token in text_processed.split():
+        if token.startswith('<'):
+            continue
+        else:
+            if token.strip() not in opinions:
+                opinions.append(token.strip())
+
+    return opinions
+    			
+
+def decode_pred_pairs(text): # pairs
+
+    starter = '<aspect>'
+    text = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    text = text.replace("<extra_id_-1>", starter).replace("<extra_id_-2>", '<opinion>')
+    text_processed = post_process(text, starter)
+    text_processed = text_processed.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+
+    current = None
+    aspect, opinion = "", ""
+    pairs = []
+    for token in text_processed.split():
+        if token == '<aspect>':
+            current = 'a'
+            if opinion != '':
+                entry = {"aspect": aspect.strip(), "opinion": opinion.strip()}
+                if entry not in pairs:
+                    pairs.append(entry)
+					
+                opinion = ""
+            aspect = ""
+
+        elif token == '<opinion>':
+            current = 'o'
+            if opinion != '':
+                entry = {"aspect": aspect.strip(), "opinion": opinion.strip()}
+                if entry not in pairs:
+                    pairs.append(entry)
+					
+                opinion = ""
+
+        else:
+            if current == 'o':
+                opinion = ' ' + token
+            else:
+                aspect = ' ' + token
+                
+    if aspect != '' and opinion != '':
+        entry = {"aspect": aspect.strip(), "opinion": opinion.strip()}
+        if entry not in pairs:
+            pairs.append(entry)
+    
+
+    return pairs
+
+def decode_pred_aesc(text): # pairs
+
+    starter = '<aspect>'
+    text = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+    text = text.replace("<extra_id_-1>", starter).replace("<extra_id_-2>", '<sentiment>')
+    text_processed = post_process(text, starter)
+    text_processed = text_processed.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+
+    current = None
+    aspect, sentiment = "", ""
+    aescs = []
+    for token in text_processed.split():
+        if token == '<aspect>':
+            current = 'a'
+            if sentiment != '':
+                entry = {"aspect": aspect.strip(), "sentiment": sentiment.strip()}
+                if entry not in aescs:
+                    aescs.append(entry)
+					
+                sentiment = ""
+            aspect = ""
+
+        elif token == '<sentiment>':
+            current = 'o'
+            if sentiment != '':
+                entry = {"aspect": aspect.strip(), "sentiment": sentiment.strip()}
+                if entry not in aescs:
+                    aescs.append(entry)
+					
+                sentiment = ""
+
+        else:
+            if current == 'o':
+                sentiment = ' ' + token
+            else:
+                aspect = ' ' + token
+                
+    if aspect != '' and sentiment != '':
+        entry = {"aspect": aspect.strip(), "sentiment": sentiment.strip()}
+        if entry not in aescs:
+            aescs.append(entry)
+    
+
+    return aescs
+
+
+def decode_pred_triplets(text): # triplet
+	
+		triplets = []
+		text = text.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+		text = text.replace("<extra_id_-1>", '<triplet>').replace("<extra_id_-2>", '<opinion>' ).replace("<extra_id_-3>", '<sentiment>')
+		text_processed = post_process(text)
+		text_processed = text_processed.replace("<s>", "").replace("<pad>", "").replace("</s>", "")
+		
+		current = None
+		aspect, opinion, sentiment = "", "", ""
+		# print(text_processed)
+		for token in text_processed.split():
+			#print(token)
+			if token == '<triplet>':
+				current = 't'
+				if sentiment != "":
+					entry = {"aspect": aspect.strip(), "opinion": opinion.strip(), "sentiment": sentiment.strip()}
+					if entry not in triplets:
+						triplets.append(entry)
+					sentiment = ""
+				aspect = ""
+
+			elif token == '<opinion>':
+				current = 'o'
+				if sentiment != "":
+					entry = {"aspect": aspect.strip(), "opinion": opinion.strip(), "sentiment": sentiment.strip()}
+					if entry not in triplets:
+						triplets.append(entry)
+					sentiment = ""
+				opinion = ""
+
+			elif token == '<sentiment>':
+				current = 's'
+				sentiment = ""
+
+			elif current == 't':
+				aspect += ' ' + token
+			elif current == 'o':
+				opinion += ' ' + token
+			elif current =='s':
+				sentiment += ' ' + token
+
+		if aspect != '' and opinion != '' and sentiment != '':
+			entry = {"aspect": aspect.strip(), "opinion": opinion.strip(), "sentiment": sentiment.strip()}
+			if entry not in triplets:
+				triplets.append(entry)
+
+		return triplets
+
+
+def get_gold_aspects(dev_target_sample): # done
+    	
+	aspect = dev_target_sample.split('|')
+	aspects_list = [asp.strip() for asp in aspect ]
+	
+	return aspects_list
+
+def get_gold_opinions(dev_target_sample): # done
+    	
+	opinions = dev_target_sample.split('|')
+	opinions_list = [op.strip() for op in opinions ]
+	
+	return opinions_list
+
+def get_gold_pairs(dev_target_sample): # done
+    	
+	pairs = dev_target_sample.split('|')
+	pairs_list = []
+	for pair in pairs:
+		d = {}
+		a, o = pair.split(';')
+		d['aspect'] = a.strip()
+		d['opinion'] = o.strip()
+		pairs_list.append(d)
+
+	return pairs_list
+
+def get_gold_aesc(dev_target_sample): # done
+    	
+	aescs = dev_target_sample.split('|')
+	aescs_list = []
+	for aesc in aescs:
+		d = {}
+		a, s = aesc.split(';')
+		d['aspect'] = a.strip()
+		d['sentiment'] = sent_map[s.strip()]
+		aescs_list.append(d)
+
+	return aescs_list
+
+
+def get_gold_triplets(dev_target_sample):
+	
+	triplets = dev_target_sample.split('|')
+	triplets_list = []
+	for triplet in triplets:
+		d = {}
+		a, o, s = triplet.split(';')
+		d['aspect'] = a.strip()
+		d['opinion'] = o.strip()
+		d['sentiment'] = sent_map[s.strip()]
+		triplets_list.append(d)
+
+	return triplets_list
+
+
+
+def is_full_match(triplet, triplets):
+
+	for t in triplets:
+		if t['aspect'].lower() == triplet["aspect"].lower() and \
+		t['opinion'].lower() == triplet['opinion'].lower() and \
+		t['sentiment'].lower() == triplet['sentiment'].lower():
+			return True
+
+	return False
+
+
+
+def get_f1_for_trainer(predictions, target, component=None):
+	
+	# print(predictions)
+	# print(target)
+
+	n = len(target)
+	assert n == len(predictions)
+
+	preds, gold = [], []  
+	for i in range(n):	
+		preds.append(decode_pred_triplets(predictions[i]))
+		gold.append(decode_pred_triplets(target[i]))
+
+	pred_count = 0
+	gold_count = 0
+	correct_count = 0
+	acc = 0
+
+	for i in range(n):
+
+		pred_aspects = list(set([t['aspect'].lower() for t in preds[i]]))
+		pred_opinions = list(set([t['opinion'].lower() for t in preds[i]]))
+		gold_aspects = list(set([t['aspect'].lower() for t in gold[i]]))
+		gold_opinions = list(set([t['opinion'].lower() for t in gold[i]]))
+
+		if component == 'aspect':
+			pred_count += len(pred_aspects)
+			gold_count += len(gold_aspects)
+			correct_count += len(list(set(pred_aspects).intersection(set(gold_aspects))))
+
+		elif component == 'opinion':
+			pred_count += len(pred_opinions)
+			gold_count += len(gold_opinions)
+			correct_count += len(list(set(pred_opinions).intersection(set(gold_opinions))))
+
+		elif component == 'sentiment':
+			pair_count = 0
+			triplet_count = 0
+			for g in gold[i]:
+				for p in preds[i]:
+					if g['aspect'] == p['aspect'] and g['opinion'] == p['opinion']:
+						pair_count += 1
+						if g['sentiment'] == p['sentiment']:
+							triplet_count += 1
+			acc += 0 if pair_count == 0 else float(triplet_count)/(pair_count)
+
+		elif component is None:
+			pred_count += len(preds[i])
+			gold_count += len(gold[i])
+
+			for gt_triplet in gold[i]:
+				if is_full_match(gt_triplet, preds[i]):
+					correct_count += 1			
+
+	if component == 'sentiment':
+		acc = round(acc / n, 3)
+		return acc
+	else:
+		p = float(correct_count) / (pred_count + 1e-8 )
+		r = float(correct_count) / (gold_count + 1e-8 )
+		f1 = (2 * p * r) / (p + r + 1e-8)
+		return p, r, f1
+
+
+
+# def get_f1(predictions, target, component=None)
+
+# 	n = len(target)
+# 	assert n == len(predictions)
+
+# 	preds, gold = [], []
+# 	for i in range(n):
+# 		preds.append(decode_pred_triplets(predictions[i]))
+# 		gold.append(get_gold_triplets(target[i]))
+
+# 	pred_triplets = 0
+# 	gold_triplets = 0
+# 	correct_triplets = 0
+
+# 	for i in range(n):
+
+# 		pred_triplets += len(preds[i])
+# 		gold_triplets += len(gold[i])
+
+# 		for gt_triplet in gold[i]:
+
+# 			if component is None and is_full_match(gt_triplet, preds[i]):
+# 				correct_triplets += 1
+# 			elif component == 'aspect' and is_full_match(gt_triplet, preds[i], aspect = True):
+# 				correct_triplets += 1
+# 			elif component == 'opinion' and is_full_match(gt_triplet, preds[i], opinion = True):
+# 				correct_triplets += 1
+# 			elif component == 'sentiment' and is_full_match(gt_triplet, preds[i], sentiment = True):
+# 				correct_triplets += 1
+
+# 	p = float(correct_triplets) / (pred_triplets + 1e-8 )
+# 	r = float(correct_triplets) / (gold_triplets + 1e-8 )
+# 	f1 = (2 * p * r) / (p + r + 1e-8)
+
+# 	return p, r, f1
+
+
+
+# def is_full_match(triplet, triplets, aspect=None, opinion=None, sentiment=None):
+
+# 	for t in triplets:
+# 		if aspect:
+# 			if t['aspect'] == triplet["aspect"]:
+# 				return True
+# 		elif opinion:
+# 			if t['opinion'] == triplet['opinion']:
+# 				return True
+# 		elif sentiment:
+# 			if t['sentiment'] == triplet['sentiment']:
+# 				return True
+# 		else:
+# 			if t['aspect'] == triplet["aspect"] and t['opinion'] == triplet['opinion'] and t['sentiment'] == triplet['sentiment']:
+# 				return True
+
+# 	return False
+
+
+
+
+# def get_f1_for_trainer(predictions, target, component=None):
+	
+# 	# print(predictions)
+# 	# print(target)
+
+# 	n = len(target)
+# 	assert n == len(predictions)
+
+# 	preds, gold = [], []  
+# 	for i in range(n):	
+# 		preds.append(decode_pred_triplets(predictions[i]))
+# 		gold.append(decode_pred_triplets(target[i]))
+
+# 	pred_triplets = 0
+# 	gold_triplets = 0
+# 	correct_triplets = 0
+
+# 	for i in range(n):
+
+# 		pred_triplets += len(preds[i])
+# 		gold_triplets += len(gold[i])
+
+# 		for gt_triplet in gold[i]:
+
+# 			if component is None and is_full_match(gt_triplet, preds[i]):
+# 				correct_triplets += 1
+# 			elif component == 'aspect' and is_full_match(gt_triplet, preds[i], aspect=True):
+# 				correct_triplets += 1
+# 			elif component == 'opinion' and is_full_match(gt_triplet, preds[i], opinion=True):
+# 				correct_triplets += 1
+# 			elif component == 'sentiment' and is_full_match(gt_triplet, preds[i], sentiment=True):
+# 				correct_triplets += 1
+
+# 	p = float(correct_triplets) / (pred_triplets + 1e-8 )
+# 	r = float(correct_triplets) / (gold_triplets + 1e-8 )
+# 	f1 = (2 * p * r) / (p + r + 1e-8)
+
+# 	return p, r, f1
