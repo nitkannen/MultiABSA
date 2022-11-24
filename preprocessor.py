1b87cd4dc27172a6bd4bfdd34646646699c9df77
nitkannen <nithishkannen@gmail.com>
2022-11-24 17:21:55 +0530

    removed tagger and regressor

--- preprocessor.py
+++ preprocessor.py
@@ -329,15 +329,15 @@ class ABSA_Dataset(Dataset):
 
 		sentences, tuples = read_data(self.data_path, self.k_shot, self.absa_task)
 		inputs, targets = get_transformed_data(sentences, tuples, self.absa_task)
-		input_tags = self.get_all_tags(sentences, tuples)  ### pad this and letzgoooo
-		trip_counts = self.count_triplets(tuples)
+		# input_tags = self.get_all_tags(sentences, tuples)  ### pad this and letzgoooo
+		# trip_counts = self.count_triplets(tuples)
 
 		for i in range(len(inputs)):
 
 			input = inputs[i]
 			target = targets[i]
-			input_tag = input_tags[i]
-			trip_count = trip_counts[i]
+			#input_tag = input_tags[i]
+			#trip_count = trip_counts[i]
 
 			tokenized_input = self.tokenizer(
 			  [input], max_length=self.max_len, pad_to_max_length=True, truncation=True,
@@ -350,11 +350,11 @@ class ABSA_Dataset(Dataset):
 				return_tensors="pt"
 			)
 
-			input_tag = input_tag + [0] * (self.max_len - len(input_tag))
-			input_tag = torch.tensor(input_tag)
-			trip_count = torch.tensor(trip_count)
+			# input_tag = input_tag + [0] * (self.max_len - len(input_tag))
+			# input_tag = torch.tensor(input_tag)
+			# trip_count = torch.tensor(trip_count)
 
-			self.input_tags.append(input_tag)
+			#self.input_tags.append(input_tag)
 			self.inputs.append(tokenized_input)
 			self.targets.append(tokenized_target)
-			self.trip_counts.append(trip_count)
+			#self.trip_counts.append(trip_count)
